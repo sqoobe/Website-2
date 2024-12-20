f818f0f4e89f21bc42a51611e292290babd4eec5
sqoobe <sqoobe96@gmail.com>
2024-12-20 13:51:10 +0100

    javascript for sidebar

---
+++ index.js
@@ -0,0 +1,27 @@
+const toggleButton = document.getElementById('toggle-btn')
+const sidebar = document.getElementById('sidebar')
+
+//closes sidebar
+function toggleSidebar(){
+    sidebar.classList.toggle('close')
+    toggleButton.classList.toggle('rotate') //rotate icon
+
+    //closes dropdown menu when toggle sidebar
+    Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
+        ul.classList.remove('show')
+        ul.previousElementSibling.classList.remove('rotate')
+    })
+}
+
+//show dropdown menu list
+function toggleSubMenu(button){
+    button.nextElementSibling.classList.toggle('show')
+    button.classList.toggle('rotate')//rotate icon
+
+
+    //opens sidebar if you click a dropdown menu when sidebar closed
+    if(sidebar.classList.contains('close')){
+        sidebar.classList.toggle('close')
+        toggleButton.classList.toggle('rotate')
+    }
+}
